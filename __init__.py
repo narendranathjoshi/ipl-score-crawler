11015c92202501d0ae1b375f2be371e82bebfc8e
nikithanagaraj <nikki.nikkinag.nag@gmail.com>
2013-10-24 19:41:25 +0530

    for loop done

--- __init__.py
+++ __init__.py
@@ -1,6 +1,20 @@
 import urllib.request
-response = urllib.request.urlopen('http://www.iplt20league.com/ipl_6/rcb_vs_mi_scorecard_match_2.html')
-html = response.read()
-f = open('test.html', 'w')
-print(html, file=f)
-print("Done")
+
+"""
+match1= urllib.request.urlopen('http://www.iplt20league.com/ipl_6/kkr_vs_dd_scorecard_match_1.html')
+match2= urllib.request.urlopen('http://www.iplt20league.com/ipl_6/rcb_vs_mi_scorecard_match_2.html')
+match3= urllib.request.urlopen('http://www.iplt20league.com/ipl_6/srh_vs_pwi_scorecard_match_3.html')
+"""
+
+listmatches=[match1,match2,match3]
+
+
+for i in range(len(listmatches)):
+    ht=listmatches[i].read()
+    f=open('match'+str(i)+'.html','w')
+    print(ht,file=f)
+    
+print("done")
+
+
+
