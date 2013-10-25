6ba88184316d8c18dd24d628faae9ebdc7663866
preranak <preranakrishnamurthy@gmail.com>
2013-10-25 10:17:43 +0530

    match_urls and ipl-score-crawler.py updated
    1.listmatches is crested
    2.for loop is updated

--- __init__.py
+++ __init__.py
@@ -1,18 +1,14 @@
 import urllib.request
+from match_urls import listmatches
 
-"""
-match1= urllib.request.urlopen('http://www.iplt20league.com/ipl_6/kkr_vs_dd_scorecard_match_1.html')
-match2= urllib.request.urlopen('http://www.iplt20league.com/ipl_6/rcb_vs_mi_scorecard_match_2.html')
-match3= urllib.request.urlopen('http://www.iplt20league.com/ipl_6/srh_vs_pwi_scorecard_match_3.html')
-"""
 
-listmatches=[match1,match2,match3]
+#listmatches=[match1,match2,match3]
 
 
 for i in range(len(listmatches)):
     ht=listmatches[i].read()
     f=open('match'+str(i)+'.html','w')
-    print(ht,file=f)
+    print(ht, file = f)
     
 print("done")
 
