746136f38560cd3ad3a53dfcaa82f882a23c6aea
preranak <preranakrishnamurthy@gmail.com>
2013-10-25 09:59:09 +0530

    match_urls.py is updated
    All the urls with the respective match numbers are added.
    format:
    match<number> = 'url'

--- match_urls.py
+++ match_urls.py
@@ -1,1 +1,117 @@
+match1 = 'http://www.iplt20league.com/ipl_6/kkr_vs_dd_scorecard_match_1.html'
 match2 = 'http://www.iplt20league.com/ipl_6/rcb_vs_mi_scorecard_match_2.html'
+match3 = 'http://www.iplt20league.com/ipl_6/srh_vs_pwi_scorecard_match_3.html'
+match4 = 'http://www.iplt20league.com/ipl_6/dd_vs_rr_scorecard_match_4.html'
+match5 = 'http://www.iplt20league.com/ipl_6/csk_vs_mi_scorecard_match_5.html'
+match6 = 'http://www.iplt20league.com/ipl_6/pwi_vs_kxip_scorecard_match_6.html'
+match7 = 'http://www.iplt20league.com/ipl_6/srh_vs_rcb_scorecard_match_7.html'
+match8 = 'http://www.iplt20league.com/ipl_6/rr_vs_kkr_scorecard_match_8.html'
+match9 = 'http://www.iplt20league.com/ipl_6/rcb-vs-srh_scorecard_match_9.html'
+match10 = 'http://www.iplt20league.com/ipl_6/mi-vs-dd_scorecard_match_10.html'
+match11 = 'http://www.iplt20league.com/ipl_6/kxip-vs-csk_scorecard_match_11.html'
+match12 = 'http://www.iplt20league.com/ipl_6/rcb-vs-kkr_scorecard_match_12.html'
+match13 = 'http://www.iplt20league.com/ipl_6/pwi-vs-rr_scorecard_match_13.html'
+match14 = 'http://www.iplt20league.com/ipl_6/dd-vs-srh_scorecard_match_14.html'
+match15 = 'http://www.iplt20league.com/ipl_6/mi-vs-pwi_scorecard_match_15.html'
+match16 = 'http://www.iplt20league.com/ipl_6/csk-vs-rcb_scorecard_match_16.html'
+match17 = 'http://www.iplt20league.com/ipl_6/kkr_vs_srh_scorecard_match_17.html'
+match18 = 'http://www.iplt20league.com/ipl_6/rr_vs_kxip_scorecard_match_18.html'
+match19 = 'http://www.iplt20league.com/ipl_6/csk_vs_pwi_scorecard_match_19.html'
+match20 = 'http://www.iplt20league.com/ipl_6/kxip_vs_kkr_scorecard_match_20.html'
+match21 = 'http://www.iplt20league.com/ipl_6/rcb_vs_dd_scorecard_match_21.html'
+match22 = 'http://www.iplt20league.com/ipl_6/pwi_vs_srh_scorecard_match_22.html'
+match23 = 'http://www.iplt20league.com/ipl_6/rr_vs_mi_scorecard_match_23.html'
+match24 = 'http://www.iplt20league.com/ipl_6/dd_vs_csk_scorecard_match_24.html'
+match25 = 'http://www.iplt20league.com/ipl_6/srh_vs_kxip_scorecard_match_25.html'
+match26 = 'http://www.iplt20league.com/ipl_6/kkr_vs_csk_scorecard_match_26.html'
+match27 = 'http://www.iplt20league.com/ipl_6/rcb_vs_rr_scorecard_match_27.html'
+match28 = 'http://www.iplt20league.com/ipl_6/dd_vs_mi_scorecard_match_28.html'
+match29 = 'http://www.iplt20league.com/ipl_6/kxip_vs_pwi_scorecard_match_29.html'
+match30 = 'http://www.iplt20league.com/ipl_6/csk_vs_rr_scorecard_match_30.html'
+match31 = 'http://www.iplt20league.com/ipl_6/rcb_vs_pwi_scorecard_match_31.html'
+match32 = 'http://www.iplt20league.com/ipl_6/dd_vs_kxip_scorecard_match_32.html'
+match33 = 'http://www.iplt20league.com/ipl_6/kkr_vs_mi_scorecard_match_33.html'
+match34 = 'http://www.iplt20league.com/ipl_6/csk_vs_srh_scorecard_match_34.html'
+match35 = 'http://www.iplt20league.com/ipl_6/kkr_vs_kxip_scorecard_match_35.html'
+match36 = 'http://www.iplt20league.com/ipl_6/rr_vs_srh_scorecard_match_36.html'
+match37 = 'http://www.iplt20league.com/ipl_6/mi_vs_rcb_scorecard_match_37.html'
+match38 = 'http://www.iplt20league.com/ipl_6/csk_vs_kkr_scorecard_match_38.html'
+match39 = 'http://www.iplt20league.com/ipl_6/dd_vs_pwi_scorecard_match_39.html'
+match40 = 'http://www.iplt20league.com/ipl_6/rr_vs_rcb_scorecard_match_40.html'
+match41 = 'http://www.iplt20league.com/ipl_6/mi_vs_kxip_scorecard_match_41.html'
+match42 = 'http://www.iplt20league.com/ipl_6/pwi_vs_csk_scorecard_match_42.html'
+match43 = 'http://www.iplt20league.com/ipl_6/srh_vs_mi_scorecard_match_43.html'
+match44 = 'http://www.iplt20league.com/ipl_6/dd_vs_kkr_scorecard_match_44.html'
+match45 = 'http://www.iplt20league.com/ipl_6/csk_vs_kxip_scorecard_match_45.html'
+match46 = 'http://www.iplt20league.com/ipl_6/pwi_vs_rcb_scorecard_match_46.html'
+match47 = 'http://www.iplt20league.com/ipl_6/kkr_vs_rr_scorecard_match_47.html'
+match48 = 'http://www.iplt20league.com/ipl_6/srh_vs_dd_scorecard_match_48.html'
+match49 = 'http://www.iplt20league.com/ipl_6/mi_vs_csk_scorecard_match_49.html'
+match50 = 'http://www.iplt20league.com/ipl_6/rr_vs_pwi_scorecard_match_50.html'
+match51 = 'http://www.iplt20league.com/ipl_6/kxip_vs_rcb_scorecard_match_51.html'
+match52 = 'http://www.iplt20league.com/ipl_6/rr_vs_dd_scorecard_match_52.html'
+match53 = 'http://www.iplt20league.com/ipl_6/mi_vs_kkr_scorecard_match_53.html'
+match54 = 'http://www.iplt20league.com/ipl_6/srh_vs_csk_scorecard_match_54.html'
+match55 = 'http://www.iplt20league.com/ipl_6/kxip_vs_rr_scorecard_match_55.html'
+match56 = 'http://www.iplt20league.com/ipl_6/pwi_vs_kkr_scorecard_match_56.html'
+match57 = 'http://www.iplt20league.com/ipl_6/dd_vs_rcb_scorecard_match_57.html'
+match58 = 'http://www.iplt20league.com/ipl_6/pwi_vs_mi_scorecard_match_58.html'
+match59 = 'http://www.iplt20league.com/ipl_6/kxip_vs_srh_scorecard_match_59.html'
+match60 = 'http://www.iplt20league.com/ipl_6/kkr_vs_rcb_scorecard_match_60.html'
+match61 = 'http://www.iplt20league.com/ipl_6/rr_vs_csk_scorecard_match_61.html'
+match62 = 'http://www.iplt20league.com/ipl_6/mi_vs_srh_scorecard_match_62.html'
+match63 = 'http://www.iplt20league.com/ipl_6/rcb_vs_kxip_scorecard_match_63.html'
+match64 = 'http://www.iplt20league.com/ipl_6/csk_vs_dd_scorecard_match_64.html'
+match65 = 'http://www.iplt20league.com/ipl_6/kkr_vs_pwi_scorecard_match_65.html'
+match66 = 'http://www.iplt20league.com/ipl_6/mi_vs_rr_scorecard_match_66.html'
+match67 = 'http://www.iplt20league.com/ipl_6/kxip_vs_dd_scorecard_match_67.html'
+match68 = 'http://www.iplt20league.com/ipl_6/srh_vs_rr_scorecard_match_68.html'
+match69 = 'http://www.iplt20league.com/ipl_6/kxip_vs_mi_scorecard_match_69.html'
+match70 = 'http://www.iplt20league.com/ipl_6/rcb_vs_csk_scorecard_match_70.html'
+match71 = 'http://www.iplt20league.com/ipl_6/pwi_vs_dd_scorecard_match_71.html'
+match72 = 'http://www.iplt20league.com/ipl_6/srh_vs_kkr_preview_live_match_72.html'
+match73 = 'http://www.iplt20league.com/ipl_6/csk_vs_mi_scorecard_match_73.html'
+match74 = 'http://www.iplt20league.com/ipl_6/rr_vs_srh_scorecard_match_74.html'
+match75 = 'http://www.iplt20league.com/ipl_6/mi_vs_rr_scorecard_match_75.html'
+match76 = 'http://www.iplt20league.com/ipl_6/csk_vs_mi_scorecard_match_76.html'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
