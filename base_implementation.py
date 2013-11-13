7c8bdb4d5d2d485ac460e27a5d871ee4f6cc6cd2
nikithanagaraj <nikki.nikkinag.nag@gmail.com>
2013-11-13 14:21:46 +0530

    test print statements commented

--- base_implementation.py
+++ base_implementation.py
@@ -10,10 +10,10 @@ class Implementation:
     def getFiles(self):  
         self.matchesResponse = []      
         for i in (range(len(matchesURL))): #range(len(matchesURL))
-            print('a', i)
+            #print('a', i) #testing #success
             self.matchesResponse.append(request.urlopen(matchesURL[i]))
             a = self.matchesResponse[i].read()
             f = open('match' + str(i+1) + '.html', 'w')
             print(a, file = f)                     
     
-print("done")
+#print("done") #testing #success
